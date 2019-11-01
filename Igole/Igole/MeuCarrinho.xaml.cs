b16f9d450d050ff9b4baf9e759ecc3c698078673
matheus Luan de Sá <matheus.sa_@hotmail.com>
2019-11-01 13:27:57 -0400

    Terceiro Commit

---
+++ Igole/Igole/MeuCarrinho.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Igole
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MeuCarrinho : ContentPage
+    {
+        public MeuCarrinho()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Igole/Igole/Model/ModLogin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Igole.Model
+{
+    public class ModLogin /*: INotifyPropertyChanged*/
+    {
+
+
+        //public Action ExibirAvisoDeLoginInvalido;
+        //public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        //private string email;
+        //public string Email
+        //{
+        //    get { return email; }
+        //    set
+        //    {
+        //        email = value;
+        //        PropertyChanged(this, new PropertyChangedEventArgs("Email"));
+        //    }
+        //}
+        //private string senha;
+        //public string Senha
+        //{
+        //    get { return senha; }
+        //    set
+        //    {
+        //        senha = value;
+        //        PropertyChanged(this, new PropertyChangedEventArgs("Senha"));
+        //    }
+        //}
+        //public ICommand SubmitCommand { protected set; get; }
+
+
+
+        //public ModLogin()
+        //{
+        //    SubmitCommand = new Command(OnSubmit);
+        //}
+        //public void OnSubmit()
+        //{
+        //    if (email != "fasipe" || senha != "fasipe123")
+        //    {
+        //        ExibirAvisoDeLoginInvalido();
+        //    }
+        //}
+
+
+    }
+
+    
+}
+
+
